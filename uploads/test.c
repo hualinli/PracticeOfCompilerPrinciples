9f0ec30c888951f0ff05c441a222eaf8eaa0eac0
hualinli <749930355@qq.com>
2024-05-19 13:18:58 +0800

    Remove ignored files from GitHub

---
+++ uploads/test.c
@@ -0,0 +1,15 @@
+int main() {
+    1EC
+    int a;
+    a a;
+    a = 10;
+    t;
+    if(a == 10) {
+        char c;//12312
+        c = 'a';
+        printf("Hello World!%c", c);
+    }
+    while(a>=10) {
+        printf("Hello World!%c", c);
+    }
+}
